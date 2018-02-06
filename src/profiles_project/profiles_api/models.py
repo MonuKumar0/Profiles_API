8e51dda9f3e043f09795d52d7503172f1bbf250a
Monu Kumar <kumarmonu0522@gmail.com>
2018-02-06 18:40:05 +0530

    ProfileFeed

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -44,4 +44,15 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     
     
 
-# Create your models here.
+class ProfileFeedItem(models.Model):
+    
+    user_profile=models.ForeignKey('UserProfile',on_delete=models.CASCADE)
+    status_text=models.CharField(max_length=255)
+    created_on=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Return the model as string"""
+        
+        return self.status_text
+    
+    
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,9 +1,16 @@
-
+from . import models
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from . import serializers
+from rest_framework.authentication import TokenAuthentication
+from . import permissions
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
 
 class HelloApiView(APIView):
     
@@ -38,4 +45,19 @@ class HelloViewSet(viewsets.ViewSet):
                "Monu Kumar",
                 ]
         return Response({'message':'Hello', 'a_viewset':a_viewset})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
     
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name','email',)
+ 
+class LoginViewSet(viewsets.ViewSet):
+    
+    serializer_class=AuthTokenSerializer
+    
+    def create(self,request):
+        return ObtainAuthToken().post(request)
